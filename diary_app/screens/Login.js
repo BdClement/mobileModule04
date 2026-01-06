0f0625c26d676883da782827a6deba4d32077034
BdClement <probernardclement2@gmail.com>
2026-01-06 17:31:14 +0100

    feature: ex00 Github auth in progress

--- diary_app/screens/Login.js
+++ diary_app/screens/Login.js
@@ -1,4 +1,4 @@
-import { StyleSheet , View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet , View, Text, TouchableOpacity, Platform } from "react-native";
 import React, { useEffect } from 'react';
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Image } from 'expo-image'
@@ -7,12 +7,12 @@ import { supabase } from '../utils/supabaseClient';
 import * as AuthSession from 'expo-auth-session';
 import * as Linking from 'expo-linking';
 
-const redirectUri = AuthSession.makeRedirectUri({
-  useProxy: true
-});
 // const redirectUri = AuthSession.makeRedirectUri({
-//   scheme: "diary"
+//   useProxy: true
 // });
+const redirectUri = AuthSession.makeRedirectUri({
+  scheme: "diary"
+});
 console.log('Test affichage redirect', redirectUri);
 
 const handleLogin = async () => {
@@ -50,28 +50,43 @@ const handleLogin = async () => {
 };
 
 export default function LoginPage() {
-
-    useEffect(() => {
-    const handleDeepLink = async (event) => {
-      const url = event.url;
-      console.log("🔁 Redirect reçu :", url);
-  
-      // IMPORTANT : dire à Supabase de traiter l’URL
-      const { data, error } = await supabase.auth.getSessionFromUrl({ url });
-  
-      if (error) {
-        console.log("❌ Erreur session :", error);
-        return;
-      }
-  
-      console.log("✅ Session récupérée :", data.session);
-    };
-    // App ouverte via url
-    const subscription = Linking.addEventListener('url', handleDeepLink);
-  
-    return () => {
-      subscription.remove();// Nettoyage 
-    };
+  console.log("Entree dans LoginPage");
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      console.log("Entree dans WEB useEffect");
+      // Web donc l'url est dans window.location
+      const getWebSession = async () => {
+        console.log("Entree dans getWebSession");
+        const { data: { session }, error } = await supabase.auth.getSession();
+        // Est ce que error sera levee en cas de non session ?
+        if (error) console.log("Erreur session web : ", error);
+        else console.log("Session web recuperee : ", session);
+      };
+      getWebSession();
+    }
+    else {
+      console.log("Entree dans mobile useEffect");
+      const handleDeepLink = async (event) => {
+        const url = event.url;
+        console.log("🔁 Redirect reçu :", url);
+      
+        // IMPORTANT : dire à Supabase de traiter l’URL
+        const { data, error } = await supabase.auth.getSessionFromUrl({ url });
+      
+        if (error) {
+          console.log("❌ Erreur session :", error);
+          return;
+        }
+      
+        console.log("✅ Session récupérée :", data.session);
+      };
+      // App ouverte via url
+      const subscription = Linking.addEventListener('url', handleDeepLink);
+    
+      return () => {
+        subscription.remove();// Nettoyage 
+      };
+    }
   }, []);
   
   // Ecoute des changement d'auth
@@ -148,4 +163,13 @@ export default function LoginPage() {
 // Terminer Github redirection avec Linking 
 // Faire Google redirection
 // Faire page intermediraire pour laisser le chox entre Google ou Github
-// 
+
+// Tester via Mac ?
+
+
+// Fail => sdkmanager narrive pas a contacter les repo google (pb reseaus)
+// Emulateur android Sans installer directement Android Studio
+    // Telecharger l'outils de ligne de commande android sutdio
+    // Installer le SDK minimal et l'emulateur :
+        // Aller dans cmdline-tools/bin
+        // Executer "./sdkmanager --sdk_root=$HOME/Android/Sdk "platform-tools" "platforms;android-33" "system-images;android-33;google_apis;x86_64" "emulator"
